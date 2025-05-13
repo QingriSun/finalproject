e29e21f3de3d1e411ddd36bad9348006b513f997
QingriSun <13527712210@163.com>
2025-05-13 13:18:29 +0800

    remove sqr

--- sqr_finalproject/src/TestGitHub.java
+++
@@ -1,7 +0,0 @@
-package finalproject.sqr_finalproject.src;
-
-public class TestGitHub {
-    public static void main(String[] args) {
-        System.out.println("this is yyc");
-    }
-}
--- sqr_finalproject/src/controller/UserController.java
+++
@@ -1,7 +0,0 @@
-package finalproject.sqr_finalproject.src.controller;
-
-public class UserController {
-    public static boolean validateUser(String username, String password) {
-        return  true;
-    }
-}
