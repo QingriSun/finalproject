2e09c6ae1d9ac950bfbefff117e8f4adc9ffea22
QingriSun <13527712210@163.com>
2025-05-13 13:24:17 +0800

    Update TestGitHub.java

--- sqr_finalproject/src/TestGitHub.java
+++ sqr_finalproject/src/TestGitHub.java
@@ -2,6 +2,6 @@ package finalproject.sqr_finalproject.src;
 
 public class TestGitHub {
     public static void main(String[] args) {
-        System.out.println("this is yyc");
+        System.out.println("this is sqr");
     }
 }
